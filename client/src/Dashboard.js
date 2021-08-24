e7343178a9dbd89dfc13e4a1ba5ddfee1beef8ec
Eli <beer@craftdelivery.ca>
2021-08-23 21:03:31 -0400

    purge boilerplate and import router

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,5 +1,11 @@
 import Sidebar from './features/sidebar/sidebar'
 import Mobile from './features/sidebar/mobile'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
 
 export default function Dashboard() {
   return (
@@ -10,7 +16,13 @@ export default function Dashboard() {
         <div className="flex-1 relative z-0 flex overflow-hidden">
           <main className="flex-1 relative z-0 overflow-y-auto focus:outline-none xl:order-last">
             <div className="absolute inset-0 py-6 px-4 sm:px-6 lg:px-8">
-              <div className="h-full border-2 border-gray-200 border-dashed rounded-lg" />
+              <div className="h-full border-2 border-gray-200 border-dashed rounded-lg">
+                <Router>
+                  <Switch>
+                    
+                  </Switch>
+                </Router>
+              </div>
             </div>
           </main>
         </div>
--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,9 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
-//import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from '@reduxjs/toolkit'
 import sidebarReducer from '../features/sidebar/sidebarSlice'
 
 export const store = configureStore({
   reducer: {
     sidebar: sidebarReducer,
   },
-});
+})

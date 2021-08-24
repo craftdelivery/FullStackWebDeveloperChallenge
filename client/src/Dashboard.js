eb1de597ec72f51d536185a2dc529eeeab25f97d
Eli <beer@craftdelivery.ca>
2021-08-24 13:23:14 -0400

    Link in dashboard btn grp

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -35,24 +35,30 @@ export default function Dashboard() {
                     <Route path='/'>
                       <div className="flex h-screen justify-center items-center">
                         <div className="m25 text-center relative z-0 inline-flex shadow-sm rounded-md">
-                          <button
-                            type="button"
-                            className="relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
-                          >
-                            Search
-                          </button>
-                          <button
-                            type="button"
-                            className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
-                          >
-                            Update
-                          </button>
-                          <button
-                            type="button"
-                            className="-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
-                          >
-                            Remove
-                          </button>
+                          <Link to='/search'>
+                            <button
+                              type="button"
+                              className="relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+                            >
+                              Search
+                            </button>
+                          </Link>
+                          <Link to='/update'>
+                            <button
+                              type="button"
+                              className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+                            >
+                              Update
+                            </button>
+                          </Link>
+                          <Link to='/remove'>
+                            <button
+                              type="button"
+                              className="-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+                            >
+                              Remove
+                            </button>
+                          </Link>
                         </div>
                       </div>
                     </Route>

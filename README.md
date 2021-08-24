9dbba702fdd18a06a3b8f737dfd253e7d80be168
Eli <beer@craftdelivery.ca>
2021-08-24 13:17:56 -0400

    add a button group to home Dashboard

--- README.md
+++ README.md
@@ -1,10 +1,24 @@
 # Overview
 
-WIP
+A react app with a serverless backend to search, remove and update sets of words
+
+## Strategy
+
+It was clear that using postgres to perform trigram word similarity queries would be the most effective way to solve the problem, as the data needed to be stores somewhere in any case
+
+## Database Seeding
+
+see `/server/util` for scripts that clean up the data and populate the db
+
+1) `init.sql` sets up the tables, sililarity index and inserts the Man and the Sea corpus
+
+2) `gen_list.sh` generates a sorted and unique list of words found in the text
+
+3) `populate.js` inserts the words into postges using a single query using unwrap
 
 ## Client
 
-Hosted on ??
+Hosted on Netlify: [site](https://eloquent-fermat-678284.netlify.app/)
 
 A react app using @redux-tools, created with create-react-app using the redux template
 
@@ -18,8 +32,8 @@ Hosted on AWS Lambda using the serverless framework
 
 The base url is the same for all endpoints with the last segment being a query param
 
-| Action   |     Method  | Route           |
-|----------|:------------|:----------------|
-| Remove   |  /DELETE    | /search/:word   |
-| Search   |  /GET       | /search/:query  |
-| Update   |  /PUT       | /search/:word   |
+| Action   |     Method  | Route          |
+|----------|:------------|:---------------|
+| Remove   |  DELETE    | /search/:word   |
+| Search   |  GET       | /search/:query  |
+| Update   |  PUT       | /search/:word   |
--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -2,6 +2,7 @@ import Sidebar from './features/sidebar/sidebar'
 import Mobile from './features/sidebar/mobile'
 import {
   BrowserRouter as Router,
+  Link,
   Switch,
   Route,
 } from "react-router-dom"
@@ -16,7 +17,7 @@ export default function Dashboard() {
       <div className="h-screen flex overflow-hidden bg-white">
         <Sidebar />
         <div className="flex flex-col min-w-0 flex-1 overflow-hidden">
-        <Mobile />
+          <Mobile />
           <div className="flex-1 relative z-0 flex overflow-hidden">
             <main className="flex-1 relative z-0 overflow-y-auto focus:outline-none xl:order-last">
               <div className="absolute inset-0 py-6 px-4 sm:px-6 lg:px-8">
@@ -32,8 +33,27 @@ export default function Dashboard() {
                       <Update />
                     </Route>
                     <Route path='/'>
-                      <div>
-                        Default Route
+                      <div className="flex h-screen justify-center items-center">
+                        <div className="m25 text-center relative z-0 inline-flex shadow-sm rounded-md">
+                          <button
+                            type="button"
+                            className="relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+                          >
+                            Search
+                          </button>
+                          <button
+                            type="button"
+                            className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+                          >
+                            Update
+                          </button>
+                          <button
+                            type="button"
+                            className="-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+                          >
+                            Remove
+                          </button>
+                        </div>
                       </div>
                     </Route>
                   </Switch>

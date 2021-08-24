80a02ac63fc142a06d1300e1d0a99c5d1200de12
Eli <beer@craftdelivery.ca>
2021-08-24 10:50:59 -0400

    import tailwind form. tidy up styles and classes in search

--- client/src/features/search/index.js
+++ client/src/features/search/index.js
@@ -4,9 +4,11 @@ import search from '../../rest/search'
 import { clearSearch, setTerm } from './searchSlice'
 import Reslts from './results'
 
-const active = "mt-3 w-full inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-const inactive = "mt-3 w-full inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white bg-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-const reset = "bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+const styles = {
+  active: "mt-3 w-full inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm",
+  inactive: "mt-3 w-full inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white bg-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm",
+  reset: "bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded",
+}
 
 export default function Search() {
   const dispatch = useDispatch()
@@ -20,7 +22,7 @@ export default function Search() {
     }
   }, [searchTerm])
 
-  const classes = val.length ? active : inactive
+  const btn_class = val.length ? styles.active : styles.inactive
 
   const submit = () => {
     search(dispatch, val)
@@ -35,12 +37,12 @@ export default function Search() {
   return (
     <div className="bg-white shadow sm:rounded-lg">
       <div className="px-4 py-5 sm:p-6">
-        <h3 className="text-lg leading-6 font-medium text-gray-900">
+        <h3 className="m-7 text-lg leading-6 font-medium text-gray-900">
           Search
           {
             val.length ? (
               <button
-                className={`m-7 ${reset}`}
+                className={styles.reset}
                 onClick={clear}
               >
                 Clear
@@ -70,7 +72,7 @@ export default function Search() {
           <button
             onClick={submit}
             disabled={!val.length || inProgress}
-            className={classes}
+            className={btn_class}
           >
             {
               inProgress ? 'Searching...' : 'Go'
